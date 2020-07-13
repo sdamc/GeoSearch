0434bdad2f8946ce78f74d51512bd57ff891654e
MichelGoncalves5 <goncalvesmichel5@gmail.com>
2020-07-12 23:45:37 -0300

    Trying to figure out 2nd api

--- js/script.js
+++ js/script.js
@@ -8,10 +8,8 @@ btnSearch.onclick = function(){
     const url = `https://api.punkapi.com/v2/beers?food=${searchTerm}`;
     console.log(url)
     fetch(url)
-    .then(function(data){
-        return data.json();
-    })
-    .then(function(jsonObject){
+    .then(data => data.json())
+    .then(jsonObject => {
         console.log(jsonObject);
         for(beer in jsonObject){
             const beerInfo = new Array(jsonObject[beer].name, jsonObject[beer].tagline, jsonObject[beer].description, jsonObject[beer].image_url)
@@ -35,3 +33,50 @@ function beerOut(beer) {
     </div>
     </div>`;
 }
+
+//#######wine
+
+//Authorization: Token {f16aaf0eba63dc50268bae7bff67bcca58a82407}
+
+const wineSearch = document.getElementById('wineSearch');
+const wineTxt = document.getElementById('wine');
+const resultWine = document.getElementById('wine_result');
+let outWine = "";
+
+wineSearch.onclick = function(){
+    var searchTerm = wineTxt.value;
+    const url = `https://api.globalwinescore.com/globalwinescores/latest=${searchTerm}`;
+    fetch(url, {
+        mode: 'no-cors'
+    })
+    var myHeaders = new Headers();
+    myHeaders.append("Authorization", "Token {f16aaf0eba63dc50268bae7bff67bcca58a82407}")
+    .then(data => data.json())
+    .then(jsonObject => {
+        console.log(jsonObject);
+        for(wine in jsonObject){
+            const wineInfo = new Array (jsonObject[wine].wine, jsonObject[wine].wine_id, jsonObject[wine].color, jsonObject[wine].wine_type)
+
+            wineOut(wineInfo);
+        }
+        resultWine.innerHTML = out;
+    })
+    .catch(function(e){
+        console.log("Error: " + e);
+    });
+}
+
+function wineOut(wine) {
+    console.log(wine);
+    out += `<div class="beer">
+    <div class="beerImage"><img src="${beer[3]}"/></div>
+    <div class="beerText">
+        <h2>${wine[0]}</h2>
+        <h3>${wine[1]}</h3>
+        <p><em>${wine[2]}</em></p>
+    </div>
+    </div>`;
+}
+
+
+
